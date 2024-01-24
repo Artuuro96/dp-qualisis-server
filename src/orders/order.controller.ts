97b666662788f5b0982392203e6d9c8eb9114091
Arturo Rodriguez <arturorodr96@gmail.com>
2024-01-24 17:38:26 -0600

    added structure for orders api

---
+++ src/orders/order.controller.ts
@@ -0,0 +1,12 @@
+import { Controller, Get, Param } from '@nestjs/common';
+import { OrderService } from './order.service';
+
+@Controller('orders')
+export class OrderController {
+  constructor(private readonly orderService: OrderService) {}
+
+  @Get('/:id')
+  getOrderById(@Param('id') id: string): { id: string } {
+    return this.orderService.getOrderById(id);
+  }
+}
