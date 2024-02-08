7797660e83b10e34d45c19ba1fe6b7eb86a33883
Esaurs117 <124194392+Esaurs117@users.noreply.github.com>
2024-02-07 19:38:26 -0600

    Docs add Swagger documentation

--- src/dtos/client.dto.ts
+++ src/dtos/client.dto.ts
@@ -1,13 +1,20 @@
 import { IsString, IsNotEmpty, IsDefined, IsOptional } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class ClientDTO {
   @IsString()
   @IsNotEmpty()
   @IsDefined()
+  @ApiProperty({
+    required: true,
+  })
   name: string;
 
   @IsString()
   @IsNotEmpty()
   @IsOptional()
+  @ApiProperty({
+    required: false,
+  })
   description?: string;
 }
--- src/dtos/instrument.dto.ts
+++ src/dtos/instrument.dto.ts
@@ -1,28 +1,44 @@
 import { IsString, IsNotEmpty, IsDefined, IsOptional } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class InstrumentDTO {
   @IsString()
   @IsNotEmpty()
   @IsDefined()
+  @ApiProperty({
+    required: true,
+  })
   entryId: string;
 
   @IsString()
   @IsNotEmpty()
   @IsDefined()
+  @ApiProperty({
+    required: true,
+  })
   orderId: string;
 
   @IsString()
   @IsNotEmpty()
   @IsDefined()
+  @ApiProperty({
+    required: true,
+  })
   name: string;
 
   @IsString()
   @IsNotEmpty()
   @IsOptional()
+  @ApiProperty({
+    required: false,
+  })
   type?: string;
 
   @IsString()
   @IsNotEmpty()
   @IsOptional()
+  @ApiProperty({
+    required: false,
+  })
   description?: string;
 }
